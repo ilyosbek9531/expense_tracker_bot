83acd609ad6727c7b55b7f572c09865d631cba11
ilyosbek sheraliyev <ilyosbeksheraliyev@ilyosbeks-MacBook-Air.local>
2024-09-12 11:41:37 +0500

    feat: cron job added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,11 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Cron(CronExpression.EVERY_MINUTE)
+  handleCron(): void {
+    console.log('Calling the / endpoint every 1 minute');
+    this.getHello();
   }
 }
