86b61c72dcf2c103e62fe7f726c63644583e7ea6
ilyosbek sheraliyev <ilyosbeksheraliyev@ilyosbeks-MacBook-Air.local>
2024-09-23 15:05:12 +0500

    chore: deploy

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Controller()
 export class AppController {
@@ -9,11 +8,5 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-
-  @Cron(CronExpression.EVERY_MINUTE)
-  handleCron(): void {
-    console.log('Calling the / endpoint every 1 minute');
-    this.getHello();
   }
 }
